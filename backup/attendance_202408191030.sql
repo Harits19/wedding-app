519ec42a82b68675ab5bb00e72f64d46a46bf270
Abdullah Harits <harits.abdullah19@gmail.com>
2024-08-19 10:35:57 +0700

    rename folder

---
+++ backup/attendance_202408191030.sql
@@ -0,0 +1,2 @@
+INSERT INTO attendance (name,attendance,createdAt) VALUES
+	 ('qweqwe','not_attend','1720535636186');
---
+++ backup/whitelist_202408191030.sql
@@ -0,0 +1,49 @@
+INSERT INTO whitelist (name,createdAt) VALUES
+	 ('Pakde Misbah','1721198885318'),
+	 ('Pakde Pramono','1721198885318'),
+	 ('Mas Naufal','1721198885318'),
+	 ('Mas Wahyu','1721198885318'),
+	 ('Mba Ayu','1721198885318'),
+	 ('Mbah Jito','1721198885318'),
+	 ('Om Rozi','1721198885318'),
+	 ('Om Rakha','1721198885318'),
+	 ('Bulek Rahma','1721198885318'),
+	 ('Om Robbi','1721198885318');
+INSERT INTO whitelist (name,createdAt) VALUES
+	 ('Om Rendra','1721198885318'),
+	 ('Bude Ari','1721198885318'),
+	 ('Pakde Sutris','1721198885318'),
+	 ('Mas Deris','1721198885318'),
+	 ('Pakde Jangi','1721198885318'),
+	 ('Pakde Tatok','1721198885318'),
+	 ('Mba Erni','1721198885318'),
+	 ('Koko','1721198885318'),
+	 ('Pakde Aris','1721198885318'),
+	 ('Keluarga Jakarta','1721198885318');
+INSERT INTO whitelist (name,createdAt) VALUES
+	 ('Keluarga Cilacap','1721198885318'),
+	 ('Tante Putri','1721198885318'),
+	 ('Mbah Mus','1721198885318'),
+	 ('Bude tin','1721198885318'),
+	 ('Mas Furqon','1721198885318'),
+	 ('Mamah fi','1721198885318'),
+	 ('Bude Endang','1721198885318'),
+	 ('Mba Arum','1721198885318'),
+	 ('Bulek Lis','1721198885318'),
+	 ('Mba Ayu Madiun','1721198885318');
+INSERT INTO whitelist (name,createdAt) VALUES
+	 ('Om Agus','1721198885318'),
+	 ('Enggar','1721198885318'),
+	 ('Om Bambang','1721198885318'),
+	 ('Bude Nining','1721198885318'),
+	 ('Bu Amiyati','1721198885318'),
+	 ('Tante Iil','1721198885318'),
+	 ('Om Adi','1721198885318'),
+	 ('Tante Nana','1721198885318'),
+	 ('Tante Putri','1721198885318'),
+	 ('Trisila','1721198885318');
+INSERT INTO whitelist (name,createdAt) VALUES
+	 ('Nur Hanim','1721198885318'),
+	 ('Nur Hasanah','1721198885318'),
+	 ('asdas','1721273996252'),
+	 ('asdas','1721274522875');
